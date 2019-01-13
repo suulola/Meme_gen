b1584ddfc9a0ee8ba844ef930aed8d47b5c77fe3
mySuulola <oluwaseyisuulola@gmail.com>
2019-01-13 18:11:24 +0100

    Updates

---
+++ precache-manifest.4fac588848df1df2812afcda1347362e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e3687384c8ca6889f35",
+    "url": "/Meme_gen/static/js/runtime~main.4e368738.js"
+  },
+  {
+    "revision": "1c3bb2817deb05edfb72",
+    "url": "/Meme_gen/static/js/main.1c3bb281.chunk.js"
+  },
+  {
+    "revision": "49131146e316c0964e86",
+    "url": "/Meme_gen/static/js/1.49131146.chunk.js"
+  },
+  {
+    "revision": "1c3bb2817deb05edfb72",
+    "url": "/Meme_gen/static/css/main.b122caaf.chunk.css"
+  },
+  {
+    "revision": "8c90a15595d2eff672f3f379fffe16e5",
+    "url": "/Meme_gen/index.html"
+  }
+];
